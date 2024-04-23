b42d40b1f443e97335d9f1881438c4724d56f609
SangB <bbc8282@gmail.com>
2024-04-23 06:51:49 +0000

    Update: Add TestConfig Example

--- src/api_server/protocols.py
+++ src/api_server/protocols.py
@@ -13,6 +13,31 @@ class TestConfig(BaseModel):
     workload_range: Tuple[int | None, int | None] = (None, None)
     kwargs: dict = {}
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "url": "http://209.20.156.226:8000/v1",
+                    "model": "facebook/opt-125m",
+                    "dataset_name": "synthesizer",
+                    "endpoint_type": "vllm",
+                    "dataset_config": {
+                        "func": "lambda t: int(t / 0.1 + 1) if t < 30 else None",
+                        "prompt_source": "arena"
+                    },
+                    "kwargs": {
+                        "temperature": 0.9,
+                        "top_p": 1,
+                        "max_tokens": 512,
+                        "time_step": 0.01,
+                        "request_timeout": 3600
+                    }
+                }
+            ]
+        }
+    }
+    
+
     def get_model_name(self):
         return self.model.split("/")[-1]
     
